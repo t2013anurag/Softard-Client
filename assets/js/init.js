6203d287ea86890f34ee073e501f521c861a0a02
t2013anurag <tiwari.anurag126@gmail.com>
2016-03-18 11:39:25 +0530

    Ajax perfected in init

--- assets/js/init.js
+++ assets/js/init.js
@@ -39,9 +39,42 @@ $(document).ready(function(){
     });
 });
 
+$( "#user-settings" ).on( 'click', "#send-complain" ,function() {
+    var name = $("#user-name").val();
+    var email = $('#user-email').val();
+    var query = $('#query').val();
+    $.ajax({
+      type: 'POST',
+      url: '/user/complain?',
+      data: { 'name' : name, 'email' : email, 'query' : query },
+      dataType: 'json',
+      success: function(data) {
+        if(data.status === 100) {
+          var value = data.status;
+          $('#error-msg').hide();
+          $('#success-msg').show();
+          $('#user-name').val(""); //setting values to null
+          $('#user-email').val("");
+          $('#query').val("");
+        } else {
+          var value = data.status;
+          $('#error-msg').show();
+          $('#success-msg').hide();// to hide any previous success messages
+        }
+      },
+      error: function(data) {
+        var value = data.status;
+        $('#success-msg').hide();
+        $('#error-msg').show();
+      }
+  });
+});
+
+
+
 $("#profile-settings").click(function(){
     $.get('/profile-settings', function (template) {
-             $('#results123').html(template);
+             $('#user-settings').html(template);
       });
 })
 
